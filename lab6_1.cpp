00221df1ab3c19afc45b583dee5cf3eb6ded07d4
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2023-12-01 14:47:34 +0700

    Update lab6_1.cpp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int evenNum = 0;
+    int oddNum = 0;
+    int input = 1;
+    
+    while(input != 0){
+        cout << "Enter an integer: ";
+        cin >> input;
+        
+        if(input == 0){
+            break;
+        }
+        
+        if(input % 2 == 0){
+            evenNum++;
+        }
+        else{
+            oddNum++;
+        }
+    }
+    
+    cout << "#Even numbers = " << evenNum << "\n";
+    cout << "#Odd numbers = " << oddNum;
+    
     return 0;
 }
