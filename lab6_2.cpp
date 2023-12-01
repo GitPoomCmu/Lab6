7c948f6907522ddfbc8f473c072fc1e1cdcba895
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2023-12-01 15:38:35 +0700

    Update lab6_2.cpp

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+double deg2rad(double deg){
+    double rad = deg * 3.141593/180;
+    return rad;
+}
+    
+double rad2deg(double rad){
+    double deg = rad * 180/3.141593;
+    return deg;
+}
+    
+double findXComponent(double len1, double len2, double deg1, double deg2){
+    double compo = len1 * cos(deg1) + len2 * cos(deg2);
+    return compo;
+}
+
+double findYComponent(double len1, double len2, double deg1, double deg2){
+    double compo = len1 * sin(deg1) + len2 * sin(deg2);
+    return compo;
+}
+
+double pythagoras(double a, double b){
+    return sqrt(pow(a, 2) + pow(b, 2));
+}
+
+void showResult(double displayL, double displayD){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = " << displayL << "\n";
+    cout << "Direction of the resultant vector (deg) = " << displayD << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -12,6 +44,7 @@ int main(){
     cin >> l2;
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
+
     
     a1 = deg2rad(a1);
     a2 = deg2rad(a2);
